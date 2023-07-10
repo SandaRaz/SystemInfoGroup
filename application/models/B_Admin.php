a9aefc2a12774ce64535dbe0d621bdb8d99e9165
SandaRaz <25sandanirina@gmail.com>
2023-07-10 16:47:57 +0300

    Update B_Admin.php

--- application/models/B_Admin.php
+++ application/models/B_Admin.php
@@ -4,7 +4,11 @@
 class B_Admin extends CI_Model{
     public function __construct(){
         parent::__construct();
-        
+        $this->load->database();
+    }
+
+    public function checkLogin($data){
+        $query = $this->db->get_where('')
     }
 }
 
