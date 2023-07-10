377c8b6d57575be7f266dd2d7c30ac980799e7c2
SandaRaz <25sandanirina@gmail.com>
2023-07-10 17:18:56 +0300

    Update B_RegimeSport.php

--- application/models/B_RegimeSport.php
+++ application/models/B_RegimeSport.php
@@ -6,8 +6,13 @@ class B_RegimeSport extends CI_Models{
         $this->load->database();
     }
 
-    public function insertRegimeSport($libelle, ){
-        $this->db->insert('regime_sportive', array('libelle'=> $libelle,));
+    public function inserer($libelle,$action,$valeur){
+        $this->db->insert('regime_sportive', array('libelle'=> $libelle, 'action'=>$action, 'valeur'=>$valeur));
+        return $this->db->insert_id();  // si 0 >> erreur
+    }
+
+    public function modifier($id,$libelle,$action,$valeur){
+        $this->db->update('regime_sportive', array('libelle'=>$libelle,'action'=>$action, ), array('id_regime_sport'=>$id))
     }
 }
 
