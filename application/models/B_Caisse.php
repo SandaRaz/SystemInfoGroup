fbe88c0c72665943425f2731c20f300b133fbdc0
HardyRak <119031361+HardyRak@users.noreply.github.com>
2023-07-11 05:47:10 +0200

    Update B_Caisse.php

--- application/models/B_Caisse.php
+++ application/models/B_Caisse.php
@@ -31,33 +31,4 @@ class B_Caisse extends CI_Model{
         return $results;
     }
 } 
-    class B_Caisse extends CI_Model{
-        public function __construct() {
-            parent::__construct();
-            $this->load->database();
-        }
-
-        public function getMontantActuel(){
-            $data=$this->db->get('Caisse')->row();
-            return $data['montant'];
-        }
-
-        public function getListeRevenu($mois, $annee){
-            $query = "SELECT *, (benefice - depense) as revenu
-                  FROM Mvt_Caisse 
-                  WHERE benefice > depense 
-                  AND MONTH(dates) = ? 
-                  AND YEAR(dates) = ?";
-            $result = $this->db->query($query, array($mois, $annee));
-        }
-    
-        public function getListeDepense($mois, $annee){
-            $query = "SELECT *, (benefice - depense) as depense
-                  FROM Mvt_Caisse 
-                  WHERE benefice < depense 
-                  AND MONTH(dates) = ? 
-                  AND YEAR(dates) = ?";
-            $result = $this->db->query($query, array($mois, $annee));
-        }
-    }
 ?>
